b96e3a782bd587982c237d81a4873f76fe42b99a
Almir Zulic <almir@bind.ba>
2022-10-14 13:14:42 +0200

    commonjs

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-export const isValidEmail = (email) => {
+const isValidEmail = (email) => {
   const emailRegEx = /^(?:[\w!#$%&'*+\-/=?^`{|}~]+\.)*[\w!#$%&'*+\-/=?^`{|}~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
   return emailRegEx.test(email);
+};
+
+module.exports = {
+  isValidEmail,
 };
