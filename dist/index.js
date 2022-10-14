d14586d1e972a6b399a9449d2c43a7da703b54cc
Almir Zulic <almir@bind.ba>
2022-10-14 13:30:59 +0200

    publish

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+var isValidEmail = function (email) {
+    var emailRegEx = /^(?:[\w!#$%&'*+\-/=?^`{|}~]+\.)*[\w!#$%&'*+\-/=?^`{|}~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
+    return emailRegEx.test(email);
+};
+module.exports = {
+    isValidEmail: isValidEmail
+};
